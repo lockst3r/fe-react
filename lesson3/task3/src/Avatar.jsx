96bd8a69d78b012301b605e68285828805716c14
lockst3r <ushakovqq@gmail.com>
2021-01-19 00:07:23 +0200

    add

---
+++ lesson3/task3/src/Avatar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Avatar = (props) => {
+    return (
+      <img
+        className="avatar"
+        src={props.avatarUrl}
+        alt={props.name}
+      />
+    );
+};
+
+export default Avatar;
