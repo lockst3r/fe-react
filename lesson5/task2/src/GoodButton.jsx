78262d3a1f8b4d8ba09fc4df10ce5757e6b2ca93
lockst3r <ushakovqq@gmail.com>
2021-01-20 13:21:09 +0200

    add

---
+++ lesson5/task2/src/GoodButton.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.scss';
+
+
+class GoodButton extends Component {
+handleClick(e){
+alert(`${e.target.textContent}`);
+}
+render(){
+return <button className="fancy-button" onClick={this.handleClick}>Click me!</button>
+}
+}
+
+
+
+export default GoodButton;
---
+++ lesson5/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import GoodButton from "./GoodButton.jsx";
+
+const element = document.querySelector("#root");
+
+ReactDOM.render(<GoodButton />, element);
