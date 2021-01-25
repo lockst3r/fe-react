b9df410a53456ad043948be0cb257d5c466aa53c
lockst3r <ushakovqq@gmail.com>
2021-01-25 22:32:32 +0200

    add

--- lesson9/task4/src/index.jsx
+++ lesson9/task4/src/index.jsx
@@ -8,23 +8,23 @@ const rootElement = document.querySelector('#root');
 const users = [
   {
       id: 'id-0',
-      age: 29,
-      name: 'Sanya',
+      age: 21,
+      name: 'Bob',
   },
   {
       id: 'id-1',
-      age: 55,
-      name: 'Sergo',
+      age: 17,
+      name: 'Tom',
   },
   {
       id: 'id-2',
       age: 18,
-      name: 'Dyadya',
+      name: 'Tad',
   },
   {
       id: 'id-3',
       age: 45,
-      name: 'Solomon',
+      name: 'Justin',
   },
   {
       id: 'id-4',
@@ -33,14 +33,34 @@ const users = [
   },
   {
       id: 'id-5',
-      age: 43,
+      age: 45,
       name: 'John',
   },
   {
       id: 'id-6',
-      age: 49,
+      age: 45,
       name: 'Andrew',
-  }
+  },
+  {
+      id: 'id-7',
+      age: 45,
+      name: 'Pol',
+  },
+  {
+      id: 'id-8',
+      age: 45,
+      name: 'Ron',
+  },
+  {
+      id: 'id-9',
+      age: 45,
+      name: 'Harry',
+  },
+  {
+      id: 'id-10',
+      age: 45,
+      name: 'Anna',
+  },
 ];
 
 ReactDOM.render(<UsersList users={users} />, rootElement);
