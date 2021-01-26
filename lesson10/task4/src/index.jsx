d885568bea751991d471ac58c339b44770f7cd21
lockst3r <ushakovqq@gmail.com>
2021-01-26 19:51:29 +0200

    add

--- lesson10/task4/src/index.jsx
+++ lesson10/task4/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import FilterableProductTable from './FilterableProductTable.jsx';
 
 const rootElement = document.querySelector('#root');
