aecd25102ae047801a4377ebfd32624283aa3b4e
lockst3r <ushakovqq@gmail.com>
2021-01-27 13:37:52 +0200

    add

---
+++ lesson11/task1/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Numbers from './Numbers.jsx';
+import EvenNumbers from './EvenNumbers.jsx';
+import OddNumbers from './OddNumbers.jsx';
+
+class App extends Component {
+  state = {
+    number: 0,
+  };
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        number: this.state.number + 1,
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Numbers title="All numbers" number={this.state.number} />
+        <EvenNumbers title="Even numbers" number={this.state.number} />
+        <OddNumbers title="Odd numbers" number={this.state.number} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lesson11/task1/src/Numbers.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Numbers extends Component {
+  render() {
+    return (
+      <div className="number">
+        <span className="number__title">
+          {this.props.title}
+        </span>
+        <span className="number__value">
+          {this.props.number}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Numbers;
